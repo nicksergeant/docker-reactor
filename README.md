e998520a55c7365af17f7893dfd10c9f94a6c0bf
Nick Sergeant <nick@nicksergeant.com>
2014-11-12 10:30:01 -0500

    Playing with Docker and React.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ docker-reactor
 ==============
 
 Personal Docker / React playground.
+
+### Running:
+
+`docker run -p 8888:8888 nicksergeant/docker-react`
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 'use strict';
 
+var compress = require('compression');
+var ejs = require('ejs');
 var express = require('express');
+
 var app = express();
 
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.use(compress());
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/api/items', function (req, res) {
+  res.json([
+
+  ]);
+});
 app.get('/', function (req, res) {
-  res.send('Testing Hub auto-build.');
+  res.render('base.html', {});
 });
 
 app.listen(8888);
